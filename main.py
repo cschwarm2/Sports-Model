a01502031622290b9d79f5725630c8e3b58acba8
cschwarm2 <cschwarm2@gmail.com>
2024-05-13 09:37:17 -0400

    Testing

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from pprint import pprint
 import pandas as pd
-from API_Pulls import NHLAPIAccessor, NHLAPI_base
+from API_Pulls import NHLAPIAccessor, NHLAPI_base, NHLadvanced
 
 src = NHLAPIAccessor
 
-try:
-    schedule = src.getSchedule("2022-03-15")
-except:
-    print("No Schedule Avaliable")
-    schedule = None
+# try:
+#     schedule = src.getSchedule("2022-03-15")
+# except:
+#     print("No Schedule Avaliable")
+#     schedule = None
 
-print(schedule)
+# print(schedule)
 
-try:
-    stats = pd.concat([src.getBoxScore(ID) for ID in schedule['id']], ignore_index=True)
-except:
-    print("No Stats Avaliable")
-    stats = None
+# try:
+#     stats = pd.concat([src.getBoxScore(ID) for ID in schedule['id']], ignore_index=True)
+# except:
+#     print("No Stats Avaliable")
+#     stats = None
 
-print(stats)
+# print(stats)
 
+advsrc = NHLadvanced
+
+df = NHLadvanced.MPplayers('2022', 'BUF')
+print(df)
